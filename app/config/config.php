3cb4e38e2699ef3dc2cff18fdf5ec6eef41898a4
sramkellawan <sramkellawan@gmail.com>
2022-06-17 23:16:35 -0400

    first commit

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+// DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root'); //your user
+define('DB_PASS', 'syondevin1212'); // your pw
+define('DB_NAME', 'shareposts'); // your dbname
+
+// App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+// URL Root
+define('URLROOT', 'http://localhost/shareposts');// your URL
+// Site Name
+define('SITENAME', 'SharePosts'); // your sitename
+// App Version
+define('APPVERSION', '1.0.0');
